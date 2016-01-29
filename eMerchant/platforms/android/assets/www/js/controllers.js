586ed256c31ec2f74c2736325ee27ecd3ff5ef96
bobbynie <bobbynie@139.com>
2016-01-29 22:13:18 +0800

    android 编译成功

--- eMerchant/platforms/android/assets/www/js/controllers.js
+++ eMerchant/platforms/android/assets/www/js/controllers.js
@@ -1,8 +1,11 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
 
-.controller('ChatsCtrl', function($scope, Chats) {
+
+    })
+
+.controller('ChatsCtrl',['$scope', 'Chats','$cordovaBarcodeScanner','IcbcUtil',function($scope, Chats,$cordovaBarcodeScanner,IcbcUtil) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -11,17 +14,74 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = Chats.all();
+
+
+
+        $scope.scanBarcode = function() {
+
+
+            $cordovaBarcodeScanner.scan().then(function(imageData) {
+
+                alert(imageData.text);
+
+                console.log("Barcode Format -> " + imageData.format);
+
+                console.log("Cancelled -> " + imageData.cancelled);
+
+            }, function(error) {
+
+                console.log("An error happened -> " + error);
+
+            });
+
+        };
+
+
+
+        $scope.chats = Chats.all();
   $scope.remove = function(chat) {
+
+
+      IcbcUtil.getDwrMaxCallCount().then(function succ(data){
+          alert(data);
+      },function err(data){
+          alert("err:"+data);
+      });
+
     Chats.remove(chat);
   };
-})
+}] )
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
+.controller('AccountCtrl',function($scope,$q) {
 
-.controller('AccountCtrl', function($scope) {
+        function asyncGreet(name) {
+            var deferred = $q.defer();
+
+            setTimeout(function() {
+                $scope.$apply(function() {
+                    deferred.notify('即将问候 ' + name + '.');
+
+                    if (name) {
+                        deferred.resolve('你好, ' + name + '!');
+                    } else {
+                        deferred.reject('拒绝问候 ' + name + ' .');
+                    }
+                });
+            }, 1000);
+            return deferred.promise;
+        }
+
+        var promise = asyncGreet('小漠漠');
+        promise.then(function(greeting) {
+            alert('成功: ' + greeting);
+        }, function(reason) {
+            alert('失败鸟: ' + reason);
+        }, function(update) {
+            alert('收到通知: ' + update);
+        });
   $scope.settings = {
     enableFriends: true
   };
