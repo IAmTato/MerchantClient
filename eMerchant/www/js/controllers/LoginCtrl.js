09b4431a7747cb14c6b1180d5fb5ba0bbc5bda74
kin95 <kin95@126.com>
2016-02-18 19:58:32 +0800

    2016-02-18 增加登陆验证过程中遮罩

--- eMerchant/www/js/controllers/LoginCtrl.js
+++ eMerchant/www/js/controllers/LoginCtrl.js
@@ -1,9 +1,19 @@
 'use strict';
-app.controller('LoginCtrl', ['$scope', '$state', '$ionicPopup', 'AuthService', function ($scope, $state, $ionicPopup, AuthService) {
+app.controller('LoginCtrl', ['$scope', '$state','$ionicLoading', '$ionicPopup', 'AuthService',
+    function ($scope, $state,$ionicLoading, $ionicPopup, AuthService) {
   $scope.data = {};
 
+  $scope.loginStatus = false;
   $scope.login = function (data) {
+    $ionicLoading.show({
+      content: 'Loading',
+      animation: 'fade-in',
+      showBackdrop: true,
+      maxWidth: 200,
+      showDelay: 0
+    });
     AuthService.login(data.username, data.password).then(function (authenticated) {
+      $ionicLoading.hide();
       $state.go('main.dash', {}, {reload: true});
       $scope.setCurrentUsername(authenticated.userName);
     }, function (err) {
