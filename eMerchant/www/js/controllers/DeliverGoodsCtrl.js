8f742df43c7dd8437e3fb8525ed6cbf139d5545a
IAmTato <potato_Ly@163.com>
2016-02-18 13:31:20 +0800

    Tato: Deliver goods page

--- eMerchant/www/js/controllers/DeliverGoodsCtrl.js
+++ eMerchant/www/js/controllers/DeliverGoodsCtrl.js
@@ -1,52 +1,24 @@
 'use strict';
-app.controller('DeliverGoodsCtrl', ['$scope', '$state', 'HsTrMasterOrderManager', '$ionicPopup', 'AuthService', '$q',
-  function ($scope, $state, HsTrMasterOrderManager, $ionicPopup, AuthService, $q) {
-    $scope.search_key = "";
+app.controller('DeliverGoodsCtrl', ['$scope', '$state', 'ViewMasterOrderManager', '$ionicPopup', 'AuthService', '$log',
+  function ($scope, $state, ViewMasterOrderManager, $ionicPopup, AuthService, $log) {
+    
 
     $scope.logout = function () {
       AuthService.logout();
       $state.go('login');
     };
 
-    $q(function(resolve, reject) {
-      HsTrMasterOrderManager.getDeliverOrder().then(function(succ){
+      ViewMasterOrderManager.getDeliverOrder().then(function(succ){
         if(succ != null && succ.res == true ){
           $scope.fullList=succ.data;
           console.log(succ.data);
-          resolve("");
         }else{
-          reject("");
+          $log.error(succ);
         }
       },function(err){
         $log.error(err);
       });
-    });
 
-    $scope.transactionSearch = function (data) {
-      console.log("transactionSearch：Key=" + data);
-
-      var a = $q(function(resolve, reject) {
-        HsTrMasterOrderManager.getHsTrMasterOrder().then(function(succ){
-          if(succ != null && succ.res == true ){
-            console.log(succ.data);
-            $scope.fullList=succ.data;
-            resolve("");
-          }else{
-            reject("");
-          }
-        },function(err){
-          $log.error(err);
-        });
-      });
-
-      $scope.fullList = a;
-      console.log(a);
-      console.log($scope.fullList);
-      //var b = HsTrMasterOrderManager.getHsTrMasterOrder().then(function(succ){
-      //  return succ.data;
-      //});
-      //console.log(b);
-    };
 
 
     $scope.getCurrFocus = function (target) {
