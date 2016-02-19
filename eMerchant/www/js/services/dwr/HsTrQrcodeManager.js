d7acb27f5358385d88588292a01a758436f4bee6
kin95 <kin95@126.com>
2016-02-19 10:47:59 +0800

    2016-02-19 登陆超时或出错去除遮罩

---
+++ eMerchant/www/js/services/dwr/HsTrQrcodeManager.js
@@ -0,0 +1,46 @@
+"use strict";
+angular.module("$dwr")
+  .service("HsTrQrcodeManager", ["$dwr", function (dwr) {
+    var p = this;
+    (function () {
+      if (dwr.engine._getObject("HsTrQrcodeManager") == undefined) {
+
+        p = {};
+        var cn = "HsTrQrcodeManager";
+        p.setEntityManagerFactory = function (p0, cb) {
+          return dwr.$qcall.call(p, cn, 'setEntityManagerFactory', arguments, cb);
+        };
+        p.setEntityManagerFactory.className = cn;
+        p.setEntityManagerFactory.methodName = "setEntityManagerFactory";
+        p.createHsTrQrcode = function (p0, cb) {
+          return dwr.$qcall.call(p, cn, 'createHsTrQrcode', arguments, cb);
+        };
+        p.createHsTrQrcode.className = cn;
+        p.createHsTrQrcode.methodName = "createHsTrQrcode";
+        p.deleteHsTrQrcode = function (p0, cb) {
+          return dwr.$qcall.call(p, cn, 'deleteHsTrQrcode', arguments, cb);
+        };
+        p.deleteHsTrQrcode.className = cn;
+        p.deleteHsTrQrcode.methodName = "deleteHsTrQrcode";
+        p.updateHsTrQrcode = function (p0, cb) {
+          return dwr.$qcall.call(p, cn, 'updateHsTrQrcode', arguments, cb);
+        };
+        p.updateHsTrQrcode.className = cn;
+        p.updateHsTrQrcode.methodName = "updateHsTrQrcode";
+        p.findHsTrQrcodeByQrCodeId = function (p0, cb) {
+          return dwr.$qcall.call(p, cn, 'findHsTrQrcodeByQrCodeId', arguments, cb);
+        };
+        p.findHsTrQrcodeByQrCodeId.className = cn;
+        p.findHsTrQrcodeByQrCodeId.methodName = "findHsTrQrcodeByQrCodeId";
+        p.getNewHsTrQrcode = function (cb) {
+          return dwr.$qcall.call(p, cn, 'getNewHsTrQrcode', arguments, cb);
+        };
+        p.getNewHsTrQrcode.className = cn;
+        p.getNewHsTrQrcode.methodName = "getNewHsTrQrcode";
+
+        dwr.engine._setObject("HsTrQrcodeManager", p);
+      }
+    })();
+    return p;
+  }]);
+
