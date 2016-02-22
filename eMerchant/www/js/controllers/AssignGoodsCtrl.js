74dad0345a93fb2d1210ce2df759c0324e7f7efb
Vincent <fly9879@263.net>
2016-02-22 14:24:34 +0800

    修改首页显示

--- eMerchant/www/js/controllers/AssignGoodsCtrl.js
+++ eMerchant/www/js/controllers/AssignGoodsCtrl.js
@@ -2,8 +2,8 @@
  * Created by xiongfeizhao on 17/2/2016.
  */
 'use strict';
-app.controller('AssignGoodsCtrl', ['$scope', '$state', 'ViewMasterOrderManager', '$ionicPopup', 'AuthService', '$log', '$ionicLoading',
-  function ($scope, $state, ViewMasterOrderManager, $ionicPopup, AuthService, $log, $ionicLoading) {
+app.controller('AssignGoodsCtrl', ['$scope', '$state', 'ViewMasterOrderManager', '$ionicPopup', 'AuthService', '$log', '$ionicLoading', '$ionicActionSheet',
+  function ($scope, $state, ViewMasterOrderManager, $ionicPopup, AuthService, $log, $ionicLoading, $ionicActionSheet) {
 
     $scope.doRefresh = function () {
 
@@ -26,16 +26,53 @@ app.controller('AssignGoodsCtrl', ['$scope', '$state', 'ViewMasterOrderManager',
       $state.go('login');
     };
 
+    /*
     $ionicLoading.show({
       template: 'Loading...'
     });
+    */
+
+    $scope.showActionSheet = function(data) {
+      // Show the action sheet:
+      $ionicActionSheet.show({
+        buttons: [{
+          text: '訂單詳情'
+        }],
+        destructiveText: '確認已收款',
+        cancelText: '取消',
+        cancel: function() {
+        },
+        destructiveButtonClicked: function(data) {
+          var confirmPopup = $ionicPopup.confirm({
+            title: '确认订单:' + data,
+            template: '此订单为货到付款，请确认已收到款项。'
+          });
+
+          confirmPopup.then(function (res) {
+            if (res) {
+              console.log('You are sure');
+            } else {
+              console.log('You are not sure');
+            }
+          });
+          return true;
+        },
+        buttonClicked: function(index, buttonObj) {
+          switch (index) {
+            case 0:
+              alert("Clicked Share");
+              return false;
+          }
+        }
+      });
+    };
 
     ViewMasterOrderManager.getDeliverOrder().then(function (succ) {
 
       if (succ != null && succ.res == true) {
         $scope.fullList = succ.data;
         console.log(succ.data);
-        $ionicLoading.hide();
+       // $ionicLoading.hide();
       } else {
         $log.error(succ);
       }
