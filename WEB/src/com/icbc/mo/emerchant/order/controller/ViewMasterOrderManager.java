3b0347ed75a98b3dc0817369ad6b037324d30f21
IAmTato <tatolu@mc.icbc.com.cn>
2016-02-25 11:23:31 +0800

    Tato: 【當日訂單】功能 + CrossWalk

--- WEB/src/com/icbc/mo/emerchant/order/controller/ViewMasterOrderManager.java
+++ WEB/src/com/icbc/mo/emerchant/order/controller/ViewMasterOrderManager.java
@@ -1,5 +1,8 @@
 package com.icbc.mo.emerchant.order.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -17,6 +20,34 @@ import com.icbc.mo.emerchant.store.StoreToken;
 @JPAManager(targetEntity = com.icbc.mo.emerchant.order.ViewMasterOrder.class)
 public class ViewMasterOrderManager {
 
+	public class ViewMasterOrderCountsResult {
+		private String addFeeAmount;
+		private String txnCount;
+	
+		public ViewMasterOrderCountsResult() {
+		}
+	
+		public ViewMasterOrderCountsResult(String addFeeAmount, String txnCount) {
+			this.addFeeAmount = addFeeAmount;
+			this.txnCount = txnCount;
+		}
+	
+		public String getaddFeeAmount() {
+			return addFeeAmount;
+		}
+	
+		public void setaddFeeAmount(String addFeeAmount) {
+			this.addFeeAmount = addFeeAmount;
+		}
+	
+		public String getaddFeeRate() {
+			return txnCount;
+		}
+	
+		public void setaddFeeRate(String txnCount) {
+			this.txnCount = txnCount;
+		}
+	}
 	protected static final class NamedQueries {
 
 	protected static final String getViewMasterOrder = "SELECT h FROM ViewMasterOrder h WHERE h.storeId = :parm";
@@ -24,6 +55,17 @@ public class ViewMasterOrderManager {
 	//protected static final String getDeliverOrder = "SELECT h.orderId, h.currency, h.costAmount, h.realAmount, h.userName, h.createDate, h.custPhone FROM ViewMasterOrder h WHERE h.storeId = :parm and h.orderStatus in ('21')";
 	
 	protected static final String getDeliverOrder = "SELECT h FROM ViewMasterOrder h WHERE h.storeId = :parm and h.orderStatus in ('21')";
+	
+//	protected static final String getTodayFinishedOrder = "SELECT h FROM HsTrMasterOrder h WHERE h.storeId = :storeId and h.orderStatus = '31' and h.createDate >= :dateStart";
+//	protected static final String getTodayFinishedOrderCount = "SELECT sum(h.realAmount) as totalAmount, count(1) as totalCount FROM HsTrMasterOrder h WHERE h.storeId = :storeId and h.orderStatus = '31' and h.createDate >= :dateStart";//CURRENT_DATE";//to_char(h.create_date, 'YYYYMMDD') = to_char(sysdate, 'YYYYMMDD')";
+//	protected static final String getTodayFinishedOrder2 = "SELECT h.* FROM Hs_Tr_Master_Order h WHERE h.store_Id = ?1 and h.order_Status = '31' and to_char(h.create_date, 'YYYYMMDD') = to_char(sysdate, 'YYYYMMDD')";
+//	protected static final String getTodayFinishedOrderCount2 = "SELECT sum(h.real_Amount) as totalAmount, count(1) as totalCount FROM Hs_Tr_Master_Order h WHERE h.store_Id = ?1 and h.order_Status = '31' and to_char(h.create_date, 'YYYYMMDD') = to_char(sysdate, 'YYYYMMDD')";
+
+	protected static final String getTodayFinishedOrder = "SELECT h FROM ViewMasterOrder h WHERE h.storeId = :storeId and h.orderStatus = '31' and h.createDateComp >= :dateStart";
+	protected static final String getTodayFinishedOrderCount = "SELECT sum(h.realAmount) as totalAmount, count(1) as totalCount FROM ViewMasterOrder h WHERE h.storeId = :storeId and h.orderStatus = '31' and h.createDateComp >= :dateStart";//CURRENT_DATE";//to_char(h.create_date, 'YYYYMMDD') = to_char(sysdate, 'YYYYMMDD')";
+	protected static final String getTodayFinishedOrder2 = "SELECT h.* FROM View_Master_Order h WHERE h.store_Id = ?1 and h.order_Status = '31' and to_char(to_date(h.create_date, 'YYYY-MM-DD HH24:MI'), 'YYYYMMDD') = to_char(sysdate, 'YYYYMMDD')";
+	protected static final String getTodayFinishedOrderCount2 = "SELECT sum(h.real_Amount) as totalAmount, count(1) as totalCount FROM View_Master_Order h WHERE h.store_Id = ?1 and h.order_Status = '31' and to_char(h.create_date_comp, 'YYYYMMDD') = to_char(sysdate, 'YYYYMMDD')";
+
 	
 	}
 
@@ -169,4 +211,98 @@ public class ViewMasterOrderManager {
 		return results;
 	}
 
+	
+	@NamedQueryTarget("getTodayFinishedOrder")
+	public List<ViewMasterOrder> getTodayFinishedOrder(StoreToken token) {
+		
+		String storeId = token.getStoreDetail().getStoreId();
+		List<ViewMasterOrder> results = null;
+		EntityManager em = getEntityManager();
+		try {
+			Query query = em.createQuery(NamedQueries.getTodayFinishedOrder,ViewMasterOrder.class);
+			query.setParameter("storeId", storeId);
+			query.setParameter("dateStart", getDayWholePointDate(new Date()));
+			results = query.getResultList();
+			
+		} finally {
+			em.close();
+		}
+		return results;
+	}
+	
+	
+	@NamedQueryTarget("getTodayFinishedOrder2")
+	public List getTodayFinishedOrder2(StoreToken token) {
+		
+		String storeId = token.getStoreDetail().getStoreId();
+		List results = null;
+		EntityManager em = getEntityManager();
+		try {
+			Query query = em.createNativeQuery(NamedQueries.getTodayFinishedOrder2,ViewMasterOrder.class);
+			query.setParameter(1, storeId);
+			results = query.getResultList();
+			
+		} finally {
+			em.close();
+		}
+		return results;
+	}
+	
+	
+	@NamedQueryTarget("getTodayFinishedOrderCount")
+	public List<ViewMasterOrderCountsResult> getTodayFinishedOrderCount(StoreToken token) {
+		EntityManager em = getEntityManager();
+		List<ViewMasterOrderCountsResult> results = new ArrayList<ViewMasterOrderCountsResult>();
+		try {
+			String storeId = token.getStoreDetail().getStoreId();
+			Query query = em.createQuery(NamedQueries.getTodayFinishedOrderCount);
+			query.setParameter("storeId", storeId);
+			query.setParameter("dateStart", getDayWholePointDate(new Date()));
+			//results = query.getResultList();
+			List<Object[]> multipleResults = (List<Object[]>) query.getResultList();
+			for (Object[] result : multipleResults) {
+				results.add(new ViewMasterOrderCountsResult(result[0].toString(), result[1].toString()));
+			}
+		} finally {
+			em.close();
+		}
+		return results;
+	}
+
+	
+	/** 
+     * 返回今天的整点信息 
+     *  
+     * @param date 
+     * @return 2014-3-3 00:00:00 
+     */  
+    public static Date getDayWholePointDate(Date date) {  
+  
+        GregorianCalendar gc = new GregorianCalendar();  
+        gc.setTime(date);  
+        if ((gc.get(gc.HOUR_OF_DAY) == 0) && (gc.get(gc.MINUTE) == 0)  
+                && (gc.get(gc.SECOND) == 0)) {  
+            return new Date(date.getTime());  
+        } else {  
+            Date date2 = new Date(date.getTime() - gc.get(gc.HOUR_OF_DAY) * 60 * 60  
+                    * 1000 - gc.get(gc.MINUTE) * 60 * 1000 - gc.get(gc.SECOND)  
+                    * 1000);  
+            return date2;  
+        }  
+  
+    }  
+
+    
+	
+	public static void main(String[] args) {  
+        
+        Date date = new Date();  
+          
+        System.out.println(date.toString());  
+          
+        date = getDayWholePointDate(date);  
+          
+        System.out.println(date.toLocaleString());  
+          
+    }  
 }
--- eMerchant/www/js/services/dwr/ViewMasterOrderManager.js
+++ eMerchant/www/js/services/dwr/ViewMasterOrderManager.js
@@ -7,21 +7,26 @@ var p = this;
     
     p = {};
 var cn = "ViewMasterOrderManager";
-    p.setEntityManagerFactory = function(p0, cb) {
-      return dwr.$qcall.call(p,cn,'setEntityManagerFactory', arguments,cb);
+    p.main = function(p0, cb) {
+      return dwr.$qcall.call(p,cn,'main', arguments,cb);
     };
-p.setEntityManagerFactory.className = cn;
-p.setEntityManagerFactory.methodName = "setEntityManagerFactory";
-    p.getDeliverOrder = function(cb) {
-      return dwr.$qcall.call(p,cn,'getDeliverOrder', arguments,cb);
-    };
-p.getDeliverOrder.className = cn;
-p.getDeliverOrder.methodName = "getDeliverOrder";
+p.main.className = cn;
+p.main.methodName = "main";
     p.findViewMasterOrderByOrderId = function(p0, cb) {
       return dwr.$qcall.call(p,cn,'findViewMasterOrderByOrderId', arguments,cb);
     };
 p.findViewMasterOrderByOrderId.className = cn;
 p.findViewMasterOrderByOrderId.methodName = "findViewMasterOrderByOrderId";
+    p.getTodayFinishedOrderCount = function(cb) {
+      return dwr.$qcall.call(p,cn,'getTodayFinishedOrderCount', arguments,cb);
+    };
+p.getTodayFinishedOrderCount.className = cn;
+p.getTodayFinishedOrderCount.methodName = "getTodayFinishedOrderCount";
+    p.setEntityManagerFactory = function(p0, cb) {
+      return dwr.$qcall.call(p,cn,'setEntityManagerFactory', arguments,cb);
+    };
+p.setEntityManagerFactory.className = cn;
+p.setEntityManagerFactory.methodName = "setEntityManagerFactory";
     p.createViewMasterOrder = function(p0, cb) {
       return dwr.$qcall.call(p,cn,'createViewMasterOrder', arguments,cb);
     };
@@ -47,6 +52,26 @@ p.getNewViewMasterOrder.methodName = "getNewViewMasterOrder";
     };
 p.getViewMasterOrder.className = cn;
 p.getViewMasterOrder.methodName = "getViewMasterOrder";
+    p.getDayWholePointDate = function(p0, cb) {
+      return dwr.$qcall.call(p,cn,'getDayWholePointDate', arguments,cb);
+    };
+p.getDayWholePointDate.className = cn;
+p.getDayWholePointDate.methodName = "getDayWholePointDate";
+    p.getTodayFinishedOrder = function(cb) {
+      return dwr.$qcall.call(p,cn,'getTodayFinishedOrder', arguments,cb);
+    };
+p.getTodayFinishedOrder.className = cn;
+p.getTodayFinishedOrder.methodName = "getTodayFinishedOrder";
+    p.getTodayFinishedOrder2 = function(cb) {
+      return dwr.$qcall.call(p,cn,'getTodayFinishedOrder2', arguments,cb);
+    };
+p.getTodayFinishedOrder2.className = cn;
+p.getTodayFinishedOrder2.methodName = "getTodayFinishedOrder2";
+    p.getDeliverOrder = function(cb) {
+      return dwr.$qcall.call(p,cn,'getDeliverOrder', arguments,cb);
+    };
+p.getDeliverOrder.className = cn;
+p.getDeliverOrder.methodName = "getDeliverOrder";
     
     dwr.engine._setObject("ViewMasterOrderManager", p);
   }
