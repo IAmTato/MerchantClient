6166ed6aeb013946c4f5ad1fca9a7b21e41d49af
IAmTato <potato_Ly@163.com>
2016-05-11 14:32:51 +0800

    rename emerchant floder

---
+++ MerchantClient/www/js/services/AuthInterceptor.js
@@ -0,0 +1,16 @@
+'use strict';
+app.factory('AuthInterceptor', function ($rootScope, $q, AUTH_EVENTS) {
+  return {
+    responseError: function (response) {
+      $rootScope.$broadcast({
+        401: AUTH_EVENTS.notAuthenticated,
+        403: AUTH_EVENTS.notAuthorized
+      }[response.status], response);
+      return $q.reject(response);
+    }
+  };
+});
+
+app.config(function ($httpProvider) {
+  $httpProvider.interceptors.push('AuthInterceptor');
+});
